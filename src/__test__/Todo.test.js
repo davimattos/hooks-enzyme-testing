43635eb3639706e6c5f16798453e7174a907c301
davimattos <davimattosdev@gmail.com>
2019-11-29 16:04:20 -0300

    adicionando um item ao todo

--- src/__test__/Todo.test.js
+++ src/__test__/Todo.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 import Todo from "../Todo";
+import expectExport from "expect";
 
 describe("Todo", () => {
   it("renders", () => {
@@ -10,5 +11,19 @@ describe("Todo", () => {
   it("display initial to-dos", () => {
     const wrapper = mount(<Todo />);
     expect(wrapper.find("li")).toHaveLength(2);
+  });
+
+  it("adds a new item", () => {
+    const wrapper = mount(<Todo />);
+    wrapper.find("input").instance().value = "Fix failing test";
+    expect(wrapper.find("input").instance().value).toEqual("Fix failing test");
+    wrapper.find('[type="submit"]').simulate("click");
+    expect(wrapper.find("li")).toHaveLength(3);
+    expect(
+      wrapper
+        .find("li div span")
+        .last()
+        .text()
+    ).toEqual("Fix failing test");
   });
 });
